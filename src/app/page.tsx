5a6d59e8974a0607e689251730e240f42c28aa88
Matheus Monteiro <monteiromatheus047@gmail.com>
2024-02-28 21:48:27 -0300

    update: improved side icon usability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,59 +3,50 @@ import Plx from "react-plx";
 import "../../public/assets/styles/main.css";
 import "../../public/assets/images/background.jpg";
 import Image from "next/image";
+import { useEffect } from "react";
 
-export default function App() {
+export default function Home() {
+
+  const scrollHome = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    console.log("teswe");
+  }
+
+
+  useEffect(() => { scrollHome() })
   return (
     <div>
-      {
-        <Plx
-          parallaxData={[
-            {
-              start: 0,
-              end: 700,
-              easing: "easeIn",
-              properties: [
-                {
-                  startValue: 1,
-                  endValue: 1.6,
-                  property: "scale",
-                },
-              ],
-            },
-          ]}
-          style={{
-            position: "fixed",
-            left: 0,
-            top: 0,
-            width: "100%",
-            zIndex: 100,
-          }}
-        >
-          <Image
-            style={{ width: "100%" }}
-            src="/assets/images/bg.png"
-            alt="background"
-            width={200}
-            height={850}
-          />{" "}
-        </Plx>
-      }
       <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 800,
-            properties: [
-              {
-                startValue: 1,
-                endValue: 1.18,
-                property: "scale",
-              },
-            ],
-          },
+        parallaxData={[{
+          start: 0,
+          end: 300,
+          properties: [
+            {
+              startValue: 1,
+              endValue: 2.5,
+              property: "scale",
+            },
+            {
+              startValue: 1,
+              endValue: 3,
+              property: "brightness",
+            },
+          ],
+        },
+        {
+          start: 230,
+          duration: 300,
+          properties: [
+            {
+              startValue: 1,
+              endValue: 0,
+              property: "opacity",
+            },
+          ],
+        },
         ]}
         style={{
-          position: "relative",
+          position: "fixed",
           left: 0,
           top: 0,
           width: "100%",
@@ -73,12 +64,22 @@ export default function App() {
         parallaxData={[
           {
             start: 0,
-            end: 400,
+            end: 200,
             properties: [
               {
-                startValue: 1,
-                endValue: 0,
-                property: "opacity",
+                startValue: -2,
+                endValue: 816,
+                property: "translateX",
+              },
+              {
+                startValue: -34,
+                endValue: -242,
+                property: "translateY",
+              },
+              {
+                startValue: 5.6,
+                endValue: 1,
+                property: "scale",
               },
             ],
           },
@@ -91,42 +92,15 @@ export default function App() {
         }}
       >
         <Image
-          style={{ width: "35%" }}
           src="/assets/images/atlas.webp"
           alt="background"
-          width={38}
-          height={260}
+          width={160}
+          height={82}
+          onClick={() => { scrollHome() }}
+
+          style={{ cursor: "pointer" }}
         />
       </Plx>
-      <div
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          zIndex: 200,
-          paddingTop: "56%",
-          height: "400vh",
-          width: "100%",
-        }}
-      >
-        <div
-          style={{
-            background: "#000",
-            height: "100%",
-          }}
-        ></div>
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>TESTE</p>
     </div>
   );
 }
